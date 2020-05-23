24228dabd9ca8e4e7ce273fdd53bc83ed66aac84
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-23 21:32:38 +0200

    Add auto mapper profile

---
+++ Core/Configuration/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ShopWarehouse.API.Data.Dto.Product;
+using ShopWarehouse.API.Data.Models;
+
+namespace ShopWarehouse.API.Core.Configuration
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Product, ProductDto>();
+        }
+    }
+}
