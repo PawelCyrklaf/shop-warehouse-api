5aeea33c89e3399ecb346a273b31f69d0fe401be
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-07-04 22:34:18 +0200

    Remove bearer from required parameters for develop

--- Core/AddRequireHeaderParameter.cs
+++ Core/AddRequireHeaderParameter.cs
@@ -23,7 +23,7 @@ namespace ShopWarehouse.API.Core
                     Type = "String",
                     Default = new OpenApiString("Bearer ")
                 },
-                Required = true
+                Required = false
             });
         }
     }
